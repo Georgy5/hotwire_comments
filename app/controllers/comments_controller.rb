b11b303040981d7a019de3791a655b1bcf5b6b3a
Karl Keller <kgk86@hotmail.com>
2024-06-02 14:27:31 +0200

    Add Comments Controller logic

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,29 @@
 class CommentsController < ApplicationController
+  before_action :set_post
   before_action :authenticate_user!
 
   def create
+    @comment = @post.comments.new(comment_params.merge(user: current_user))
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to post_url(@post), notice: "Comment was successfully created." }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 end
